61ed648ca785e41acf5c4406c86695d4bcd80e47
한경만 <rudaks94@gmail.com>
2019-05-10 21:53:35 +0900

    GeoLocation state

---
+++ seasons/src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			lat: null,
+			long: null,
+			errorMessage: 'Loading...'
+		};
+
+		window.navigator.geolocation.getCurrentPosition(
+			position => {
+				this.setState({
+					lat: position.coords.latitude,
+					long: position.coords.longitude,
+					errorMessage: ''
+				});
+			},
+			err => {
+				this.setState({
+					errorMessage: err.message
+				});
+			}
+		);
+	}
+
+	render() {
+		if (this.state.errorMessage && !this.state.lat) {
+			return <div>Error: {this.state.errorMessage}</div>
+		}
+
+		if (!this.state.errorMessage && this.state.lat) {
+			return <div>Latitude: {this.state.lat}</div>;
+		}
+
+		return <div>Loading...</div>
+	}
+}
+
+ReactDom.render(<App/>, document.querySelector("#root"));
