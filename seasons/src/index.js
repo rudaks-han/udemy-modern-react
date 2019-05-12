ef531f8d6e5b1ec19a7115a2c3b459e01da139fd
한경만 <rudaks94@gmail.com>
2019-05-12 21:25:16 +0900

    SeasonDisplay class Component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,42 +1,36 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
+	state = { lat: null, errorMessage: '' };
 
-		this.state = {
-			lat: null,
-			long: null,
-			errorMessage: 'Loading...'
-		};
-
+	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
-			position => {
-				this.setState({
-					lat: position.coords.latitude,
-					long: position.coords.longitude,
-					errorMessage: ''
-				});
-			},
-			err => {
-				this.setState({
-					errorMessage: err.message
-				});
-			}
+			position => this.setState({lat: position.coords.latitude})	,
+			err => this.setState({errorMessage: err.message})
 		);
 	}
 
-	render() {
+	renderContent() {
 		if (this.state.errorMessage && !this.state.lat) {
 			return <div>Error: {this.state.errorMessage}</div>
 		}
 
 		if (!this.state.errorMessage && this.state.lat) {
-			return <div>Latitude: {this.state.lat}</div>;
+			return <SeasonDisplay lat={this.state.lat} />
 		}
 
-		return <div>Loading...</div>
+		return <Spinner message="Please accept location request"/>
+	}
+
+	render() {
+		return (
+			<div className="border red">
+				{this.renderContent()}
+			</div>
+		)
 	}
 }
 
