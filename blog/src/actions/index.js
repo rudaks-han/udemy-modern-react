ede63ae1a855497795d0c9d86c50f3ac4fb80c47
한경만 <rudaks94@gmail.com>
2019-05-22 07:16:27 +0900

    streams

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -3,7 +3,6 @@ import jsonPlaceHolder from '../apis/jsonPlaceHolder';
 
 export const fetchPosts = () => async dispatch => {
 	const response = await jsonPlaceHolder.get('/posts');
-
 	dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
@@ -13,4 +12,4 @@ const _fetchUser = _.memoize(async (id, dispatch) => {
 	const response = await jsonPlaceHolder.get(`/users/${id}`);
 
 	dispatch({ type: 'FETCH_USER', payload: response.data });
-})
+});
